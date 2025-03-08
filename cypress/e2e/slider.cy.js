ef0c81bddaf40a467b53c105f1288abdb916bdfb
Zoltw <filipk@remitly.com>
2025-03-08 17:53:01 +0100

    new stuff

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,21 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Navigation E2E Test', () => {
+  it('allows navigation using next and previous buttons', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.swiper-slide-active').invoke('text').then((initialText) => {
+      cy.get('.swiper-button-next').click();
+      cy.wait(2000);
+      cy.get('.swiper-slide-active').invoke('text').should((newText) => {
+        expect(newText).not.to.equal(initialText);
+      });
+
+      cy.get('.swiper-button-prev').click();
+      cy.wait(2000);
+      cy.get('.swiper-slide-active').invoke('text').should('equal', initialText);
+    });
+  });
+});
